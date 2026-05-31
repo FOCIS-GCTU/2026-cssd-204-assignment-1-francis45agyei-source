8ce781d7311b8f4df917aea72bc0950f5bb8835e
francis45agyei-source <francis45agyei@gmail.com>
2026-05-31 20:53:26 +0000

    Add files via upload

---
+++ Francis Asiedu Agyei 2425403399.py
@@ -0,0 +1,34 @@
+# File: initials.py
+# Description: Print out my initials in large letters.
+# Assignment Number: 1
+#
+# Name: Francis Agyei Asiedu
+# STUDENT ID: 2425403399
+# Email:francis45agyei@gmail.com
+# Grader: Carolyn
+#
+# On my honor, Francis Agyei Asiedu, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Prints my initials in large stylized letters
+
+    print()
+    print("...FAA")
+    print()
+
+    print("...FFFFFFFFFFFF...AAAAAAAAAAAA...AAAAAAAAAAAA...****")
+    print("...F..........F...A..........A...A..........A...****")
+    print("...F..........F...A....AA....A...A....AA....A...****")
+    print("...FFFFFFFFFFFF...A...A..A...A...A...A..A...A...****")
+    print("...F..............A..A....A..A...A..A....A..A...****")
+    print("...F..............A.A......A.A...A.A......A.A...****")
+    print("...F..............AA........AA...AA........AA...****")
+    print("...F..............AAAAAAAAAAAA...AAAAAAAAAAAA...****")
+    print("...F..............A..........A...A..........A...****")
+    print("...F..............A..........A...A..........A...****")
+    print()
+
+
+main()
